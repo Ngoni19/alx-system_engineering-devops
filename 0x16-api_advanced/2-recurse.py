e33d5955efb2713ce482324c9537476b0dde2ace
Ngonidzashe Brandon Towindo <ngoni@live.com>
2023-03-01 01:23:19 +0200

    Update 2-recurse.py

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """
 Script --> Query Reddit API recursively for
-all hot articles of a given subreddit;If no results are found;
+all hot articles of a given subreddit;If no res are found;
 for the given subreddit, the function should return None.
 """
 import requests
@@ -15,20 +15,20 @@ def recurse(subreddit, hot_list=[], aft="tmp"):
     headers = requests.utils.default_headers()
     headers.update({'User-Agent': 'My User Agent 1.0'})
 
-    # update url each recursive call with param "aft"
-    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+    # URL update--> each recursive call with param "aft"
+    URL = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
     if aft != "tmp":
-        url = url + "?aft={}".format(aft)
-    r = requests.get(url, headers=headers, allow_redirects=False)
+        URL = URL + "?aft={}".format(aft)
+    r = requests.get(URL, headers=headers, allow_redirects=False)
 
     # append top titles to hot_list[]
-    results = r.json().get('data', {}).get('children', [])
-    if not results:
+    res = r.json().get('data', {}).get('children', [])
+    if not res:
         return hot_list
-    for e in results:
-        hot_list.append(e.get('data').get('title'))
+    for i in res:
+        hot_list.append(i.get('data').get('title'))
 
-    # get next param "aft" else nothing else to recurse
+    # get next params "aft" else nothing to recurse
     aft = r.json().get('data').get('aft')
     if not aft:
         return hot_list
