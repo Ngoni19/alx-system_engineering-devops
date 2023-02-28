ab6fdc8cd41ab7eaf2b08d9c4d2f37a275236483
Ngonidzashe Brandon Towindo <ngoni@live.com>
2023-03-01 01:19:19 +0200

    Update 2-recurse.py

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -1,34 +1,35 @@
 #!/usr/bin/python3
 """
-Script--> Query Reddit API recursively;
-for all hot articles of a given subreddit
+Script --> Query Reddit API recursively for
+all hot articles of a given subreddit;If no results are found;
+for the given subreddit, the function should return None.
 """
 import requests
 
 
-def recurse(subreddit, hot_list=[], nxt="tmp"):
+def recurse(subreddit, hot_list=[], aft="tmp"):
     """
-        Return--> all hot articles given subreddit
-        & return--> None if invalid subreddit given
+        Return--> all hot articles for a given subreddit
+        &return; None if invalid subreddit given
     """
     headers = requests.utils.default_headers()
     headers.update({'User-Agent': 'My User Agent 1.0'})
 
-    # URL update each recursive call with params "nxt"
-    URL = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
-    if nxt != "tmp":
-        URL = URL + "?nxt={}".format(nxt)
-    r = requests.get(URL, headers=headers, allow_redirects=False)
+    # update url each recursive call with param "aft"
+    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+    if aft != "tmp":
+        url = url + "?aft={}".format(aft)
+    r = requests.get(url, headers=headers, allow_redirects=False)
 
-    # appending the top titles to hot_list[]
-    res = r.json().get('data', {}).get('children', [])
-    if not res:
+    # append top titles to hot_list[]
+    results = r.json().get('data', {}).get('children', [])
+    if not results:
         return hot_list
-    for i in res:
-        hot_list.append(i.get('data').get('title'))
+    for e in results:
+        hot_list.append(e.get('data').get('title'))
 
-    # get next parameter "nxt" else nothing else to recurse
-    nxt = r.json().get('data').get('nxt')
-    if not nxt:
+    # get next param "aft" else nothing else to recurse
+    aft = r.json().get('data').get('aft')
+    if not aft:
         return hot_list
-    return (recurse(subreddit, hot_list, nxt))
+    return (recurse(subreddit, hot_list, aft))
